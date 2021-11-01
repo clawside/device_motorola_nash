aef00686c2864ca1abf490af23a398b69de02f10
clawside <enyerplayus@gmail.com>
2021-11-01 00:53:13 +0000

    nash: Rebrand to POSP
    Signed-off-by: clawside <enyerplayus@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_nash.mk
+    $(LOCAL_DIR)/potato_nash.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_nash-user \
-    lineage_nash-userdebug \
-    lineage_nash-eng
+    potato_nash-user \
+    potato_nash-userdebug \
+    potato_nash-eng
